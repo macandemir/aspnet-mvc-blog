fe98a6867551496f33f01b6806cee465b9732964
macandemir <a.mehmetcandemir@gmail.com>
2023-06-03 19:04:41 +0300

    Controller ve View dosyalarının oluşturulması

---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/Admin/HomeAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers.Admin
+{
+    public class HomeAdminController : BaseAdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/Admin/UserAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers.Admin
+{
+    public class UserAdminController : BaseAdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
